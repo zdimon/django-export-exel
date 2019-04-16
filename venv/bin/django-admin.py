b2b7da20ce12fea6a18f31a43413d928955cc9b7
zdimon <zdimon@example.com>
2019-04-16 11:02:51 +0300

    auto commit

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/zdimon/storage2/home/zarik/www/wezom/django-export-exel/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
